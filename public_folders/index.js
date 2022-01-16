5703dc41fd6bdfd8930a1cdc562a1751e7e19b7e
Rusketh <Rusketh@gmail.com>
2022-01-16 22:28:16 +0000

    test

--- public_folders/index.js
+++ public_folders/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
 const port = process.env.PORT;
+const folders = process.env.FOLDERS;
 
 console.log(`Starting public folders service on ${port}`);
+
+console.log(`Folders: `, folders);
--- public_folders/run.sh
+++ public_folders/run.sh
@@ -2,7 +2,9 @@
 
 set +u
 
-export PORT=$(bashio::addon.port 4123)
+export PORT=$(bashio::addon.port 8080)
+
+export FOLDERS=$(bashio::config 'folders')
 
 bashio::log.info "Starting http service on."
 
