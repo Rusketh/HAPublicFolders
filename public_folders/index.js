3d7550557bffb9eb5aa0670d29902526d614fb78
Rusketh <Rusketh@gmail.com>
2022-01-16 23:17:17 +0000

    more testing

--- public_folders/index.js
+++ public_folders/index.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
 
-const port = process.env.PORT;
-const folders = process.env.FOLDERS;
+const PORT = process.env.PORT;
+const FOLDERS = process.env.FOLDERS;
+
+const directories = { };
+
+for (let str in FOLDERS.split("\n"))
+{
+	let split = str.split(":");
+	directories[slpit[1]] = split[2];
+}
 
 console.log(`Starting public folders service on ${port}`);
 
-console.log(`Folders: `, folders);
+console.log(`Serving public folders:`, directories);
+
+
