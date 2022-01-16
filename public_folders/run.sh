3a23db1b4fa10a3bd9d24993dc3e6e9f6b631f5d
Rusketh <Rusketh@gmail.com>
2022-01-16 21:33:15 +0000

    testing

--- public_folders/run.sh
+++ public_folders/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
-PORT = "$(bashio::addon.port '4123')"
+PORT = $(bashio::addon.port 4123)
 
 bashio::log.info "Starting public folders service on {PORT}"
 
-npm run start  PORT
+npm run start PORT
