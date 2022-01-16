9c039e89fe5714302683b39fac266f4ee45286c3
Rusketh <Rusketh@gmail.com>
2022-01-16 21:49:26 +0000

    test

--- public_folders/run.sh
+++ public_folders/run.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
-declare port
-
-port=$(bashio::addon.port 4123)
+PORT=$(bashio::addon.port 4123)
 
 bashio::log.info "Starting public folders service on {port}"
 
-# Don't execute this when SSH is disabled
-if ! bashio::var.has_value "${port}"; then
-    bashio::log.info 'No network port is defined in the configuration'.
-    exit 0
-fi
-
-npm run start port
+npm run start PORT
