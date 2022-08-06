399fc9784aa20ee7c2333c06003f76d1bbe08263
Rusketh <Rusketh@gmail.com>
2022-08-06 01:04:29 +0100

    Attempting to fix for v3.

--- public_folders/rootfs/etc/services.d/public_folders/run.sh
+++ public_folders/rootfs/etc/services.d/public_folders/run.sh
@@ -5,6 +5,6 @@ set +u
 export PORT=$(bashio::addon.port 8080)
 export FOLDERS=$(bashio::config "folders")
 
-bashio::log.info "Starting http service on."
+bashio::log.info "Starting http service on port $PORT."
 
-exec npm run start;
+exec /usr/bin/public_folders/start;
