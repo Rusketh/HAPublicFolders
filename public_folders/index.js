ebf0063ac99cb1691b1f9d14205c5c8f7186fc27
Rusketh <rusketh@gmail.com>
2022-10-30 20:32:39 +0000

    Added Directory Listing option

--- public_folders/index.js
+++ public_folders/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 //Config
 const PORT = process.env.PORT;
 const FOLDERS = process.env.FOLDERS;
+const LISTDIRS = process.env.DIRECTORYLISTING;
 
 //Directory Mappings
 const directories = { };
@@ -38,14 +39,39 @@ const handler = (req, res) => {
 	if (!base) return error(req, res, "404");
 
 	let filepath = path.join(location, ...parts);
+	
+	fs.lstat(filepath, (err, stat) => {
+		if (err) return error(req, res, "401");
+		
+		if (stat.isFile()) {
+			return fs.readFile(filepath, (err, data) => {
+				if (err && err.code == 'ENOENT') return error(req, res, "404")
+				if (err) return error(req, res, "401");
 
-    fs.readFile(filepath, (err, data) => {
-        if (err && err.code == 'ENOENT') return error(req, res, "404")
-  		if (err) return error(req, res, "401");
-       
-        res.writeHead(200);
-        res.end(data, 'utf-8');
-    });
+				res.writeHead(200);
+				res.end(data, 'utf-8');
+			});
+		}
+		
+		if (stat.isDirectory()) {
+			if (!LISTDIRS) return error(req, res, "403");
+			
+			fs.readdir(filepath, (err, files) => {
+				if (err) return error(req, res, "401");
+				
+				res.writeHead(200);
+				res.write(`<a href="${path.join(location, [...parts].splice(0, parts.length-1))}">../</a>`);
+					  
+				for (let file of files) {
+					res.write(`<br/><a href="${path.join(filepath, file)}">${file}</a>`);
+				}
+				
+				res.end();
+			});
+		}
+		
+		return error(req, res, "404");
+	});
 };
 
 //Create http server.
